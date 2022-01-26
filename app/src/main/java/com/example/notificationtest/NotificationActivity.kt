94794936e6c188a40efc9dbc43685f284a40bb47
VmYs <ysfrank1@gmail.com>
2022-01-26 14:13:51 +0800

    20220126-9.2

---
+++ app/src/main/java/com/example/notificationtest/NotificationActivity.kt
@@ -0,0 +1,18 @@
+package com.example.notificationtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.notificationtest.databinding.ActivityMainBinding
+import com.example.notificationtest.databinding.ActivityNotificationBinding
+
+class NotificationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNotificationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNotificationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
